875da3c5b7b9b68fcc881e98326ecfa9a13e2644
Mohamed Mahmoud <mohamedmahmoud@Mohameds-burhaniy-MacBook-Pro.local>
2019-09-18 17:52:33 +0200

    testing

---
+++ 15_storage_script.sh
@@ -0,0 +1,11 @@
+# ! bin/bash
+
+# Terminal script for sync/synchronizing current local directory with Amazon bucket
+# acl Options are private, public-read, or public-read-write
+
+#aws s3 sync . s3://50testingstatic  --acl  private
+
+#git pull
+
+python valmis.py 
+
--- valmis.py
+++
@@ -1,6 +0,0 @@
-
-understanding  = “ testing of management “
-with open(‘understanding.txt’,’w’) as delete:
-       delete.write( understanding) 
-delete.close()
-
