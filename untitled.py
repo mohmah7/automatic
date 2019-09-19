912d628242adb8b927687adfdca584258f7af7b4
Mohamed Mahmoud <mohamedmahmoud@Mohameds-burhaniy-MacBook-Pro.local>
2019-09-19 05:20:51 +0200

    test

--- untitled.py
+++ untitled.py
@@ -1,7 +1,47 @@
 embassy = str("hggfddsaerryuu exams")
 
-with open ("fulfillmen.txt",'w') as management:
-	management.write(embassy)
+rivers = []
+counter = []
+identification = 1
+ocean = 1
+boyfriend = 0
+Hamilton = []
+chicken = []
+cucumber = []
+while len(rivers ) < 400:
+          #ocean  += 1
+          boyfriend = ocean + boyfriend
+          rivers.append(boyfriend)
+          Hamilton.append(ocean)
+          ocean += 1 
+          #print( ocean, boyfriend)
+print (max(rivers) )
 
-management.close()
+
+for  event in rivers :
+  for tomato in  Hamilton:
+    if event % tomato == 0:
+      chicken.append(tomato)
+  #cucumber.append(chicken)
+  #print (chicken) 
+  if len(chicken) == 6: 
+      print (event, max(chicken ))
+      with open ("fulfillmen.txt",'w') as management:
+      	management.write(str (event))
+      	management.write(str("\n"))
+      	management.write(str(max(chicken)))
+      management.close()
+
+      exit ()
+  chicken = []
+  #for each  in cucumber:
+    # if len(each)==5:
+    #  print(len(each),event)
+    #  exit()
+
+
+
+
+
+
 exit()
