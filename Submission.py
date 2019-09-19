b9d5bd2b6ce198fa161cd90f09debfd58e3d9778
Mohamed Mahmoud <mohamedmahmoud@Mohameds-burhaniy-MacBook-Pro.local>
2019-09-19 16:14:10 +0200

    uodates before deletig

--- Submission.py
+++ Submission.py
@@ -13,18 +13,23 @@ while len(rivers ) <= 200000 :
           Hamilton.append(ocean)
           ocean += 1 
 
-##print( Hamilton)
-Hamilton = list( range (1, 10000, 1))
+#print( Hamilton)
+print( "--------------------------------------------")
+
+#Hamilton = list( range (1, 10000, 1))
 #print (Hamilton )
 counter =0
 
-while counter <=100000:
+while counter <=100000000:
      #counter =0
      for item in rivers:
            for identification in Hamilton:
                  if item % identification ==0 and chicken.count( identification) == 0:
                              chicken.append(identification)
-           if len(chicken) >= 500: 
+           if len(chicken) >= 460: 
+                 print(rivers.index(item))
+                 print(rivers[rivers.index(item)-1])
+                 print(rivers.index(28))
                  print (item, chicken )
                  print(len(chicken ))
                  exit()
